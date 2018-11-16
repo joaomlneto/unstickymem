c1ab8a0aa7a62c9053774bfde758fa68989e2d79
João Neto <joao@neto.pt>
2018-11-16 20:09:42 +0000

    update unstickymem with all the wrappers! wohoo!

--- src/unstickymem/unstickymem.cpp
+++ src/unstickymem/unstickymem.cpp
@@ -1,6 +1,7 @@
 
 #include <unistd.h>
 #include <dlfcn.h>
+#include <stdarg.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
 
@@ -16,10 +17,10 @@
 #include "unstickymem/unstickymem.h"
 #include "unstickymem/PerformanceCounters.hpp"
 #include "unstickymem/PagePlacement.hpp"
-#include "unstickymem/MemoryMap.hpp"
 #include "unstickymem/Logger.hpp"
 #include "unstickymem/wrap.hpp"
 #include "unstickymem/Runtime.hpp"
+#include "unstickymem/memory/MemoryMap.hpp"
 #include "unstickymem/mode/Mode.hpp"
 
 static bool OPT_NUM_WORKERS = false;
@@ -59,7 +60,7 @@ __attribute__((constructor)) void libunstickymem_initialize(void) {
   memory = &MemoryMap::getInstance();
 
   // start the runtime
-  runtime = new Runtime();
+  runtime = &Runtime::getInstance();
 
   is_initialized = true;
   LDEBUG("Initialized");
@@ -68,7 +69,7 @@ __attribute__((constructor)) void libunstickymem_initialize(void) {
 // library destructor
 __attribute((destructor)) void libunstickymem_finalize(void) {
   // stop all the counters
-  // stop_all_counters();
+  stop_all_counters();
   LINFO("Finalized");
 }
 
@@ -94,6 +95,117 @@ void unstickymem_print_memory(void) {
 
 // Wrapped functions
 
+void *malloc(size_t size) {
+  static bool inside_unstickymem = false;
+
+  // dont do anything fancy if library is not initialized
+  if (!unstickymem::is_initialized || inside_unstickymem) {
+    return ((void* (*)(size_t))dlsym(RTLD_NEXT, "malloc"))(size);
+  }
+
+  // handle the function ourselves
+  inside_unstickymem = true;
+  void *result = unstickymem::memory->handle_malloc(size);
+  inside_unstickymem = false;
+  LTRACEF("malloc(%zu) => %p", size, result);
+  return result;
+}
+
+// XXX this is a hack XXX
+// this is to solve the recursion in calloc -> dlsym -> calloc -> ...
+static unsigned char buffer[1024*1024];
+
+void *calloc(size_t nmemb, size_t size) {
+  static bool inside_unstickymem = false;
+  static bool inside_dlsym = false;
+
+  // check if we are inside dlsym -- return a temporary buffer for it!
+  if (inside_dlsym) {
+    return buffer;
+  }
+
+  // dont do anything fancy if library is not initialized
+  if (!unstickymem::is_initialized || inside_unstickymem) {
+    inside_dlsym = true;
+    void *result = ((void* (*)(size_t, size_t))
+      dlsym(RTLD_NEXT, "calloc"))(nmemb, size);
+    inside_dlsym = false;
+    return result;
+  }
+
+  // handle the function ourselves
+  inside_unstickymem = true;
+  void *result = unstickymem::memory->handle_calloc(nmemb, size);
+  inside_unstickymem = false;
+  LTRACEF("calloc(%zu, %zu) => %p", nmemb, size, result);
+  return result;
+}
+
+void *realloc(void *ptr, size_t size) {
+  static bool inside_unstickymem = false;
+
+  // dont do anything fancy if library is not initialized
+  if (!unstickymem::is_initialized || inside_unstickymem) {
+    return ((void *(*)(void*, size_t)) dlsym(RTLD_NEXT, "realloc"))(ptr, size);
+  }
+
+  // handle the function ourselves
+  inside_unstickymem = true;
+  void *result = unstickymem::memory->handle_realloc(ptr, size);
+  LTRACEF("realloc(%p, %zu) => %p", ptr, size, result);
+  inside_unstickymem = false;
+  return result;
+}
+
+void *reallocarray(void *ptr, size_t nmemb, size_t size) {
+  static bool inside_unstickymem = false;
+  // dont do anything fancy if library is not initialized
+  if (!unstickymem::is_initialized || inside_unstickymem) {
+    return ((void *(*)(void*, size_t, size_t))
+      dlsym(RTLD_NEXT, "reallocarray"))(ptr, nmemb, size);
+  }
+  // handle the function ourselves
+  inside_unstickymem = true;
+  void *result = unstickymem::memory->handle_reallocarray(ptr, nmemb, size);
+  LTRACEF("reallocarray(%p, %zu, %zu) => %p", ptr, nmemb, size, result);
+  inside_unstickymem = false;
+  return result;
+}
+
+void free(void *ptr) {
+  static bool inside_unstickymem = false;
+  // check if this is the temporary buffer passed to dlsym (see calloc)
+  if (ptr == buffer) {
+    return;
+  }
+  // dont do anything fancy if library is not initialized
+  if (!unstickymem::is_initialized || inside_unstickymem) {
+    //LWARN("bypassing our free");
+    return ((void (*)(void*)) dlsym(RTLD_NEXT, "free"))(ptr);
+  }
+  // handle the function ourselves
+  // LWARNF("running our own free because flag is %s", inside_unstickymem ? "T" : "F");
+  inside_unstickymem = true;
+  unstickymem::memory->handle_free(ptr);
+  LTRACEF("free(%p)", ptr);
+  inside_unstickymem = false;
+}
+
+int posix_memalign(void **memptr, size_t alignment, size_t size) {
+  // dont do anything fancy if library is not initialized
+  if (!unstickymem::is_initialized) {
+    return ((int (*)(void**, size_t, size_t))
+      dlsym(RTLD_NEXT, "posix_memalign"))(memptr, alignment, size);
+  }
+  // handle the function ourselves
+  int result = unstickymem::memory->handle_posix_memalign(memptr, alignment,
+                                                          size);
+  LTRACEF("posix_memalign(%p, %zu, %zu) => %d",
+          memptr, alignment, size, result);
+  return result;
+}
+
+
 void *mmap(void *addr, size_t length, int prot,
            int flags, int fd, off_t offset) {
   // dont do anything fancy if library is not initialized
@@ -104,11 +216,29 @@ void *mmap(void *addr, size_t length, int prot,
   // handle the function ourselves
   //std::shared_ptr<unstickymem::Mode> mode = unstickymem::runtime->getMode();
   //void *result = mode->mmap(addr, length, prot, flags, fd, offset);
-  void *result = unstickymem::memory->mmap(addr, length, prot, flags, fd, offset);
+  void *result = unstickymem::memory->handle_mmap(addr, length, prot, flags,
+                                                  fd, offset);
   LTRACEF("mmap(%p, %zu, %d, %d, %d, %d) => %p",
           addr, length, prot, flags, fd, offset, result);
   // return the result
   return result;
+}
+
+int munmap(void *addr, size_t length) {
+  // dont do anything fancy if library is not initialized
+  if (!unstickymem::is_initialized) {
+    return ((int (*)(void*, size_t))dlsym(RTLD_NEXT, "munmap"))(addr, length);
+  }
+  // handle the function ourselves
+  int result = unstickymem::memory->handle_munmap(addr, length);
+  LTRACEF("munmap(%p, %zu) => %d", addr, length, result);
+  // return the result
+  return result;
+}
+
+void *mremap(void *old_address, size_t old_size, size_t new_size, int flags,
+             ... /* void *new_address */) {
+  DIE("TODO");
 }
 
 int brk(void *addr) {
@@ -120,7 +250,7 @@ int brk(void *addr) {
   // handle the function ourselves
   //std::shared_ptr<unstickymem::Mode> mode = unstickymem::runtime->getMode();
   //int result = mode->brk(addr);
-  int result = unstickymem::memory->brk(addr);
+  int result = unstickymem::memory->handle_brk(addr);
   LTRACEF("brk(%p) => %d", addr, result);
   return result;
 }
@@ -134,7 +264,7 @@ void *sbrk(intptr_t increment) {
   // handle the function ourselves
   //std::shared_ptr<unstickymem::Mode> mode = unstickymem::runtime->getMode();
   //void *result = mode->sbrk(increment);
-  void *result = unstickymem::memory->sbrk(increment);
+  void *result = unstickymem::memory->handle_sbrk(increment);
   LTRACEF("sbrk(%zu) => %p", increment, result);
   return result;
 }
@@ -151,53 +281,11 @@ long mbind(void *addr, unsigned long len, int mode,
   // handle the function ourselves
   //std::shared_ptr<unstickymem::Mode> m = unstickymem::runtime->getMode();
   //long result = m->mbind(addr, len, mode, nodemask, maxnode, flags);
-  long result = unstickymem::memory->mbind(addr, len, mode, nodemask, maxnode, flags);
+  long result = unstickymem::memory->handle_mbind(addr, len, mode, nodemask,
+                                                  maxnode, flags);
   LTRACEF("mbind(%p, %lu, %d, %p, %lu, %u) => %ld",
           addr, len, mode, nodemask, maxnode, flags, result);
   return result;
-}
-
-void *malloc(size_t size) {
-    // dont do anything fancy if library is not initialized
-    if (!unstickymem::is_initialized) {
-        return ((void* (*)(size_t))
-          dlsym(RTLD_NEXT, "malloc"))(size);
-    }
-    // handle the function ourselves
-    //std::shared_ptr<unstickymem::Mode> mode = unstickymem::runtime->getMode();
-    //void *result = mode->malloc(size);
-    void *result = unstickymem::memory->malloc(size);
-    LTRACEF("malloc(%zu) => %p", size, result);
-    return result;
-}
-
-void free(void *ptr) {
-    // dont do anything fancy if library is not initialized
-    if (!unstickymem::is_initialized) {
-        return ((void (*)(void*))
-          dlsym(RTLD_NEXT, "free"))(ptr);
-    }
-    // handle the function ourselves
-    //std::shared_ptr<unstickymem::Mode> mode = unstickymem::runtime->getMode();
-    //mode->free(ptr);
-    unstickymem::memory->free(ptr);
-    LTRACEF("free(%p)", ptr);
-    return;
-}
-
-int posix_memalign(void **memptr, size_t alignment, size_t size) {
-    // dont do anything fancy if library is not initialized
-    if (!unstickymem::is_initialized) {
-        return ((int (*)(void**, size_t, size_t))
-          dlsym(RTLD_NEXT, "posix_memalign"))(memptr, alignment, size);
-    }
-    // handle the function ourselves
-    //std::shared_ptr<unstickymem::Mode> mode = unstickymem::runtime->getMode();
-    //int result = mode->posix_memalign(memptr, alignment, size);
-    int result = unstickymem::memory->posix_memalign(memptr, alignment, size);
-    LTRACEF("posix_memalign(%p, %zu, %zu) => %d",
-            memptr, alignment, size, result);
-    return result;
 }
 
 #ifdef __cplusplus
